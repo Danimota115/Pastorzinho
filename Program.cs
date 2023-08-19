f0c9bd64bc608ac2bf6c409effd3305f6770cc0c
Danimota115 <dmota6693@gmail.com>
2023-08-18 21:58:26 -0300

    finalizar o programa

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("DÓ,"); Console.Beep(1320, 200);  
+Console.WriteLine("RÉ,"); Console.Beep(1485, 200);  
+Console.WriteLine("MI,"); Console.Beep(1650, 200);  
+Console.WriteLine("FÁ,"); Console.Beep(1759, 200);  
+Console.WriteLine("FÁ,"); Console.Beep(1759, 200);  
+Console.WriteLine("FÁ,"); Console.Beep(1759, 200);
+
+Thread.Sleep(3000);
+
+Console.WriteLine("DÓ,"); Console.Beep(1320, 200);  
+Console.WriteLine("RÉ,"); Console.Beep(1485, 200);  
+Console.WriteLine("DÓ,"); Console.Beep(1320, 200);  
+Console.WriteLine("RÉ,"); Console.Beep(1485, 200);  
+Console.WriteLine("RÉ,"); Console.Beep(1485, 200); 
+Console.WriteLine("RÉ,"); Console.Beep(1485, 200); 
+Console.WriteLine("SOL,"); Console.Beep(1980, 200);  
+Console.WriteLine("FÁ"); Console.Beep(1759, 200);  
+Console.WriteLine("MI,"); Console.Beep(1650, 200);  
+Console.WriteLine("MI,"); Console.Beep(1650, 200);  
+Console.WriteLine("MI,"); Console.Beep(1650, 200);
+
+Thread.Sleep(2000);
+
+Console.WriteLine("DÓ"); Console.Beep(1320, 200);  
+Console.WriteLine("RÉ,"); Console.Beep(1485, 200);  
+Console.WriteLine("MI,"); Console.Beep(1650, 200);
+Console.WriteLine("FÁ,"); Console.Beep(1759, 200);
+Console.WriteLine("FÁ,"); Console.Beep(1759, 200);
+Console.WriteLine("FÁ,"); Console.Beep(1759, 200);
